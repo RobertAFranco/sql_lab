5f04e9a95d52f950277da361bea355305fbeacec
RobertAFranco <bigbobontheradio@gmail.com>
2024-09-09 16:45:09 -0400

    missed ;

--- dues.sql
+++ dues.sql
@@ -13,8 +13,8 @@ LIMIT 1;
 SELECT language
 FROM countrylanguages
 WHERE countrycode = (SELECT code
-                     FROM countries
-                     WHERE name = 'Holy See (Vatican City State)');
+FROM countries
+WHERE name = 'Holy See (Vatican City State)');
 
 
 
@@ -26,20 +26,22 @@ SELECT countries.name
 FROM countries
 JOIN countrylanguages ON countries.code = countrylanguages.countrycode
 WHERE countrylanguages.language = 'Italian'
-  AND countrylanguages.isofficial = TRUE
-  AND countrylanguages.percentage > 99
+AND countrylanguages.isofficial = TRUE
+AND countrylanguages.percentage > 99;
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
 SELECT name
 FROM cities
-WHERE countrycode = (SELECT code
-                     FROM countries
-                     WHERE name = 'San Marino')
-  AND name <> (SELECT name
-               FROM countries
-               WHERE name = 'San Marino');
+WHERE countrycode = 
+(SELECT code
+FROM countries
+WHERE name = 'San Marino')
+AND name <> 
+(SELECT name
+FROM countries
+WHERE name = 'San Marino');
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
@@ -48,10 +50,11 @@ WHERE countrycode = (SELECT code
 SELECT name
 FROM cities
 WHERE name LIKE 'Serra%'
-  AND countrycode IN (SELECT code
-                      FROM countries
-                      WHERE continent = 'South America')
-  AND name NOT LIKE 'Serravalle';
+AND countrycode 
+IN (SELECT code
+FROM countries
+WHERE continent = 'South America')
+AND name NOT LIKE 'Serravalle';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
@@ -62,10 +65,9 @@ SELECT cities.name
 FROM cities
 JOIN countries ON cities.id = countries.capital
 WHERE countries.code = (
-    SELECT cities.countrycode
-    FROM cities
-    WHERE cities.name = 'Serra'
-);
+SELECT cities.countrycode
+FROM cities
+WHERE cities.name = 'Serra');
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
